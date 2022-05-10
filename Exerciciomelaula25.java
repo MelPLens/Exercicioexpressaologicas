287a5a04a9900faada55ee48be9c1c62b44679b2
MEL PLENS ANGELIS <99374140+MelPLens@users.noreply.github.com>
2022-05-09 22:14:16 -0300

    Update Exerciciomelaula25.java

--- Exerciciomelaula25.java
+++ Exerciciomelaula25.java
@@ -5,30 +5,30 @@ class exerciciologicos
 }
 
 System.out.print(" Ola ! Escolha umas as opcoes abaixo caso voce esteja dentro dessas condicoes./n Digite 1 se voce for idoso /n Digite 2 se voce for possui alguma deficiencia /n Digite 3 se voce for uma gestante /n Digite 4 caso voce nao esteja dentro em nenhuma condicoes dessas. ");
-Scanner ler = new Scanner (System.in);
-int numero1;
+Scanner ler = new Scanner (System.in); //ler as instrucoes
+int numero1; // incluir variaveis inteiras
 
-if(numero1=1)
+if(numero1=1) //se algo for ...
 {
    System.out.print("Voce pode estacionar na vaga especial!");
    numero1=ler.nextInt;
 }
-else if (numero1=2)
+else if (numero1=2) //se nao se
 {
    System.out.print ("Voce pode estacionar na vaga especial!");
    numero1=ler.nextInt;
 }
-else if (numero1=3)
+else if (numero1=3) //se nao se
 {
     System.out.print ("Voce pode estacionar na vaga especial!");
     numero1=ler.nextInt;
 }
-else if (numero1=4)
+else if (numero1=4) // se nao  se
 {
     System.out.print ("Voce nao pode estacionar na vaga especial!");
     numero1=ler.nextInt;
-}
-else
+} 
+else //se nao
 {
     System.out.print ("Digite uma das opcoes 1,2,3 e 4 !");
     numero1=ler.nextInt;
